9fadca43b921074f6399c8158ebe6aa91f1e5e93
divyavavilthota <vavilthotadivya@gmail.com>
2024-06-20 21:52:28 +0530

    first

---
+++ course.sql
@@ -0,0 +1,18 @@
+ phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 30, 2022 at 05:35 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE TABLE `bookmark` (
+  `user_id` varchar(20) NOT NULL,
+  `playlist_id` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
